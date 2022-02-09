d83f872e8bf593c0f8daa57597034490b8afbfd5
sedaahi <ahiseda@gmail.com>
2022-02-09 10:27:12 +0300

    Add project files.

---
+++ BizimMarket_AreaOrnegi/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BizimMarket_AreaOrnegi.Areas.Admin.Controllers
+{
+    [Area("Admin")]  //Burası admin controllerının areası dedik
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BizimMarket_AreaOrnegi/Models/Kategori.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BizimMarket_AreaOrnegi.Models
+{
+    public class Kategori
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public string Ad { get; set; }
+
+        public List<Urun> Urunler { get; set; }  //Bir kategorinin birden fazla ürünü 
+    }
+}
